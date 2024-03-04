844eea2841424a6c19f0f9eae5f811a71cfd2c47
ljaybarr <159211043+ljaybarr@users.noreply.github.com>
2024-03-04 17:06:09 -0600

    Add files via upload

--- temperature/script/temperature.js
+++ temperature/script/temperature.js
@@ -1,10 +1,19 @@
-function convertCtoF(){
+function convertTemperature(){
 
-    let celsius=Number(prompt("Enter Celsius Temperature"));
-    
-    let temperature=((celsius*9/5)+32);
-    
-    document.getElementById("temperature").innerHTML+=`
-    <p>The conversion of Celsius ${celsius}° to Fahrenheit is ${temperature}°</p>
-    `;
+    let scale = prompt("Enter a scale: 1)F 2)C");
+
+    if(scale==1){
+        let fahrenheit = Number(prompt("Enter a number"));
+        celsius = (fahrenheit - 32) * 5/9;
+        document.getElementById("temperature").innerHTML+=`
+        <p>The conversion of Fahrenheit ${fahrenheit}° to Celsius is ${celsius}°</p>
+        `;
+    }
+    if(scale==2){
+        let celsius = Number(prompt("Enter a number"));
+        fahrenheit = (celsius * 9/5) + 32;
+        document.getElementById("temperature").innerHTML+=`
+        <p>The conversion of Celsius ${celsius}° to Fahrenheit is ${fahrenheit}°</p>
+        `;
+    }
 }
